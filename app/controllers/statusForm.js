463857113523552372be3bf4e6953391bff63098
nithina <nithina@qburst.com>
2016-03-14 18:37:02 +0530

    merge different modules into single one

--- app/controllers/statusForm.js
+++ app/controllers/statusForm.js
@@ -1,23 +1,26 @@
-angular.module('qbApp.status', [])
-        .controller("StatusFormCtrl", ['$scope', '$http', 'generateNo', 'generateDays',
+app.controller("StatusFormCtrl", ['$scope', '$http', 'generateNo', 'generateDays',
     '$routeParams', function ($scope, $http, generateNo, generateDays, $routeParams) {
-                $scope.params = $routeParams;
-                $http.get('config/project.json').success(function (data) {
-                    $scope.projects = data.Projects;
-                });
-                $http.get('config/activity-type.json').success(function (data) {
-                    $scope.activityTypes = data.Types;
-                    $scope.data.statusActivity = $scope.activityTypes[0].activity;
-                });
-                
-                daysArray = generateDays.days(8);
-                $scope.data = {
-                    statusHrs: '08',
-                    statusProject: 'na',
-                    statusMins: '00',
-                    statusDate: daysArray[0],
-                    generateDates: daysArray,
-                    generateHrs: generateNo.paddednumbers(24),
-                    generateMins: generateNo.paddednumbers(60),
-                }
-            }])
+        $scope.params = $routeParams;
+        $http.get('config/project.json').success(function (data) {
+            $scope.projects = data.Projects;
+        });
+        $http.get('config/activity-type.json').success(function (data) {
+            $scope.activityTypes = data.Types;
+            $scope.data.statusActivity = $scope.activityTypes[0].activity;
+        });
+
+        daysArray = generateDays.days(8);
+        $scope.data = {
+            statusHrs: '08',
+            statusProject: 'na',
+            statusMins: '00',
+            statusDate: daysArray[0],
+            generateDates: daysArray,
+            generateHrs: generateNo.paddednumbers(24),
+            generateMins: generateNo.paddednumbers(60),
+        }
+
+        $scope.update = function (data) {
+            $scope.submitted = true;
+        };
+    }])
